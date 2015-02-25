127d3cb3e00eab276ab0515ad4f0fb90dbd814ea
kevinwan1991 <kevinwan1991@hotmail.com>
2015-02-25 10:59:54 -0500

    2B-update

---
+++ Telemetered/2B/full-ingest-ce-glider.sh
@@ -0,0 +1,295 @@
+#!/bin/bash
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg CE05MOAS-GL311-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg CE05MOAS-GL311-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg CE05MOAS-GL311-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg CE05MOAS-GL311-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg CE05MOAS-GL311-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg CE05MOAS-GL312-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg CE05MOAS-GL312-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg CE05MOAS-GL312-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg CE05MOAS-GL312-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg CE05MOAS-GL312-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg CE05MOAS-GL319-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg CE05MOAS-GL319-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg CE05MOAS-GL319-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg CE05MOAS-GL319-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg CE05MOAS-GL319-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg CE05MOAS-GL320-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg CE05MOAS-GL320-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg CE05MOAS-GL320-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg CE05MOAS-GL320-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg CE05MOAS-GL320-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg CE05MOAS-GL381-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg CE05MOAS-GL381-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg CE05MOAS-GL381-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg CE05MOAS-GL381-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg CE05MOAS-GL381-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg CE05MOAS-GL382-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg CE05MOAS-GL382-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg CE05MOAS-GL382-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg CE05MOAS-GL382-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg CE05MOAS-GL382-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg CE05MOAS-GL383-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg CE05MOAS-GL383-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg CE05MOAS-GL383-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg CE05MOAS-GL383-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg CE05MOAS-GL383-05-CTDGVM000
+
+# Ingest.glider-eng-glider_telemetered   /omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg CE05MOAS-GL386-00-ENG000000
+# Ingest.parad-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg CE05MOAS-GL386-01-PARADM000
+# Ingest.flort-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg CE05MOAS-GL386-02-FLORTM000
+# Ingest.dosta-abcdjm-glider_telemetered /omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg CE05MOAS-GL386-04-DOSTAM000
+# Ingest.ctdgv-m-glider_telemetered      /omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg CE05MOAS-GL386-05-CTDGVM000
+
+INGS=/home/developer/uframes/ooi/bin/ingestsender
+INGESTLOG=/home/wdk/race/log/new-ingest-gp-glider-command.log
+#INGESTLOGT=/home/developer/ingest_initial/log/com.log
+INGESTLOGT=/home/wdk/race/log/new-ingest-gp-glider-command.log-T
+EDEXSRV=/home/developer/uframes/ooi/bin/edex-server
+source $EDEXSRV
+
+
+EDEXLOGDIR=/home/developer/uframes/ooi/uframe-1.0/edex/logs
+EDEXLOGFILES=$(ls $EDEXLOGDIR/edex-ooi*.log)
+
+date
+#echo "starting ingest-ce-glider.sh"
+echo "starting full-ingest-ce-glider.sh"
+
+GL311=/omc_data/whoi/OMC/CE05MOAS-GL311/D00001/merged-from-glider/ce_311_*.mrg
+#
+GL312=/omc_data/whoi/OMC/CE05MOAS-GL312/D00001/merged-from-glider/ce_312_*.mrg
+#
+GL319=/omc_data/whoi/OMC/CE05MOAS-GL319/D00001/merged-from-glider/ce_319_*.mrg
+#
+GL320=/omc_data/whoi/OMC/CE05MOAS-GL320/D00001/merged-from-glider/ce_320_*.mrg
+#
+GL381=/omc_data/whoi/OMC/CE05MOAS-GL381/D00001/merged-from-glider/ce_381_*.mrg
+#
+GL382=/omc_data/whoi/OMC/CE05MOAS-GL382/D00001/merged-from-glider/ce_382_*.mrg
+#
+GL383=/omc_data/whoi/OMC/CE05MOAS-GL383/D00001/merged-from-glider/ce_383_*.mrg
+#
+GL386=/omc_data/whoi/OMC/CE05MOAS-GL386/D00001/merged-from-glider/ce_386_*.mrg
+#
+
+GL311_files=$(find $GL311  -print)
+#
+GL312_files=$(find $GL312  -print)
+#
+GL319_files=$(find $GL319  -print)
+#
+GL320_files=$(find $GL320  -print)
+#
+GL381_files=$(find $GL381  -print)
+#
+GL382_files=$(find $GL382  -print)
+#
+GL383_files=$(find $GL383  -print)
+#
+GL386_files=$(find $GL386  -print)
+#
+
+#define UFrame Route:
+declare -x UFR=(Ingest.glider-eng-glider_telemetered
+                Ingest.parad-m-glider_telemetered
+                Ingest.flord-m-glider_telemetered
+                Ingest.dosta-abcdjm-glider_telemetered
+                Ingest.ctdgv-m-glider_telemetered)
+
+
+#G311:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL311-00-ENG000000
+                      CE05MOAS-GL311-01-PARADM000
+                      CE05MOAS-GL311-02-FLORTM000
+                      CE05MOAS-GL311-04-DOSTAM000
+                      CE05MOAS-GL311-05-CTDGVM000)
+
+for file in $GL311_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+#G312:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL312-00-ENG000000
+                      CE05MOAS-GL312-01-PARADM000
+                      CE05MOAS-GL312-02-FLORTM000
+                      CE05MOAS-GL312-04-DOSTAM000
+                      CE05MOAS-GL312-05-CTDGVM000)
+
+for file in $GL312_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+#G319:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL319-00-ENG000000
+                      CE05MOAS-GL319-01-PARADM000
+                      CE05MOAS-GL319-02-FLORTM000
+                      CE05MOAS-GL319-04-DOSTAM000
+                      CE05MOAS-GL319-05-CTDGVM000)
+
+for file in $GL319_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+#G320:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL320-00-ENG000000
+                      CE05MOAS-GL320-01-PARADM000
+                      CE05MOAS-GL320-02-FLORTM000
+                      CE05MOAS-GL320-04-DOSTAM000
+                      CE05MOAS-GL320-05-CTDGVM000)
+
+for file in $GL320_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+
+#G381:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL381-00-ENG000000
+                      CE05MOAS-GL381-01-PARADM000
+                      CE05MOAS-GL381-02-FLORTM000
+                      CE05MOAS-GL381-04-DOSTAM000
+                      CE05MOAS-GL381-05-CTDGVM000)
+
+for file in $GL381_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+
+done
+
+#G382:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL382-00-ENG000000
+                      CE05MOAS-GL382-01-PARADM000
+                      CE05MOAS-GL382-02-FLORTM000
+                      CE05MOAS-GL382-04-DOSTAM000
+                      CE05MOAS-GL382-05-CTDGVM000)
+
+for file in $GL382_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+#G383:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL383-00-ENG000000
+                      CE05MOAS-GL383-01-PARADM000
+                      CE05MOAS-GL383-02-FLORTM000
+                      CE05MOAS-GL383-04-DOSTAM000
+                      CE05MOAS-GL383-05-CTDGVM000)
+
+for file in $GL383_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+#G386:
+#define Glider Reference Desingnator:
+declare -x GL1REFDES=(CE05MOAS-GL386-00-ENG000000
+                      CE05MOAS-GL386-01-PARADM000
+                      CE05MOAS-GL386-02-FLORTM000
+                      CE05MOAS-GL386-04-DOSTAM000
+                      CE05MOAS-GL386-05-CTDGVM000)
+
+for file in $GL386_files
+do
+    if  grep --quiet $file $EDEXLOGFILES; then
+        echo "move on" > /dev/null
+    else
+        ls -l $file
+        for i in {0..4}
+        do
+            echo "$INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered"
+                  $INGS ${UFR[i]} $file ${GL1REFDES[i]} telemetered
+        done
+        sleep 3
+    fi
+done
+
+
+date
+echo "end of full-ingest-ce-glider.sh"
